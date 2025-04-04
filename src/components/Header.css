6d54195455bd2fef1b2033340bc424d15fabf8f0
Sallis Henrique <64816023+sallishenrique@users.noreply.github.com>
2025-04-04 00:34:14 -0300

    Update Header.css

--- src/components/Header.css
+++ src/components/Header.css
@@ -1,1 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Painel de Métricas</h1>
+    </header>
+  );
+}
+
+export default Header;
 
