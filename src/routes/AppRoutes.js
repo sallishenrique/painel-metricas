437f7e628541213791f19460c5d9997b38bdc483
Sallis Henrique <64816023+sallishenrique@users.noreply.github.com>
2025-04-04 00:35:52 -0300

    Update AppRoutes.js

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Admin from '../pages/Admin';
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
 
