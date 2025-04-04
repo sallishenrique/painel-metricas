838fc89a4b45a3ad589dd8b943e2f97f78f7e04a
Sallis Henrique <64816023+sallishenrique@users.noreply.github.com>
2025-04-04 00:34:58 -0300

    Update MetricsCard.js

--- src/components/MetricsCard.js
+++ src/components/MetricsCard.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './MetricsCard.css';
+
+function MetricsCard({ title, value }) {
+  return (
+    <div className="metric-card">
+      <h3>{title}</h3>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+export default MetricsCard;
 
